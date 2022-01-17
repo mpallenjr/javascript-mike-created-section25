ab5252a0d868e53a42a3dcc8c6d15694bda05720
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-17 10:49:12 -0600

    solution added.

--- challenge.js
+++ challenge.js
@@ -169,3 +169,32 @@ if(dolphinsAverageScore > koalasAverageScore) {
 
 }
 */
+
+///// Video Solution
+
+// const scoreDolphins = (96 + 108 + 89) /3 ;
+// const scoreKoalas = (88 + 91 + 110) /3 ;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy')
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+
+// Bonus 1
+const scoreDolphins = (97 + 112 + 101) /3 ;
+const scoreKoalas = (109 + 95 + 123) / 3 ;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('Koalas win the trophy')
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log('Both win the trophy!');
+} else {
+  console.log('No one wins the tophy')
+}
